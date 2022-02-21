941fb713333d96c4df0c0b6d395877318b8a29eb
mafio <mf1969@gmail.com>
2022-02-21 08:07:43 +0100

    fix

---
+++ main/public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= getenv('APP_NAME') ?></title>
+</head>
+<body>
+<h4 style="color: darkblue">PHP 8.12, NGINX, DEBIAN </h4>
+<hr>
+<?= phpinfo() ?>
+</body>
+</html>
